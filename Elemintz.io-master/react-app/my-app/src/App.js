2b1fa94dd713979e8d4af0742b48da3c566f7491
Robert L Shirak Jr <rvd@roberts-mbp.myfiosgateway.com>
2022-01-04 20:01:47 -0500

    yer react added

--- Elemintz.io-master/react-app/my-app/src/App.js
+++ Elemintz.io-master/react-app/my-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          i love you bitch, imma never stop loving you, biiiiiiiiitch
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
